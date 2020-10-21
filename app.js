4ad1aa5415836f9b3a274f307041b386e099476c
Jatin Ramchandani <jatinramchandani307@gmail.com>
2020-10-22 02:24:54 +0530

    register ,login done | profile in progress

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app=express();
 const fs=require('fs');
 const mysql=require('mysql');
 const Joi=require('joi');
+const multer=require('multer'); 
 
 app.use('/public',express.static(path.join(__dirname,'static')));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+app.set('view engine','ejs');
 
 
 const db=mysql.createConnection({
@@ -31,40 +34,10 @@ db.connect((err)=>{
 
 })
 
-
-app.get('/login',(req,res)=>{
-    let Email=req.header.Email;
-    let Password=req.header.Password;
-    var sql="SELECT Name FROM `users` WHERE `Email`='"+Email+"' and Password = '"+Password+"'";
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    });
-});
-
-
 app.get('/',(req,res)=>{
     console.log(req.params);
    res.sendFile(path.join(__dirname,'static','UserRegForm.html'));
 })
-
-
-// app.post('/',(req,res)=>{
-//    console.log(req.body);
-//     const schema=Joi.object().keys({
-//         name:Joi.string().trim().required(),
-//         Email:Joi.string().trim().email().required(),
-//         password:Joi.string().min(5).max(10).required()
-
-//     });
-//     schema.validate(req.body);
-//     //database work here
-//     res.json({success:true});
-// })
-app.listen(3000);
-
-
 
 app.post('/signup',(req,res)=>{
     
@@ -79,10 +52,85 @@ app.post('/signup',(req,res)=>{
     })
 })
 
-app.get('/LoginForm',(req,res)=>{
+app.get('/loginForm',(req,res)=>{
     console.log(req.params);
     res.sendFile(path.join(__dirname,'static','login.html'));
+});
+
+
+
+app.get('/login',(req,res)=>{
+    let Email=req.query.Email;
+    let Password=req.query.Password;
+    var sql="SELECT FName,LName,Email FROM users WHERE Email = ? AND Password = ?";
+    db.query(sql,[Email,Password],(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.render("profile",{username:result});
+    });
+});
+
+
+
+app.get("/search",(req,res)=>{
+    let nuser=req.query.searchuser;
+
+    var sql="SELECT FName,LName FROM users WHERE FName= ? ";
+    db.query(sql,nuser,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.render("others",{username:result});
+    })
 })
+
+
+const handleError = (err, res) => {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong!");
+  };
+
+  const upload = multer({
+    dest: "./images"
+  });
+
+
+app.post("/upload",upload.single("file"),(req,res)=>{
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./images/Screenshot (440).png");
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+        fs.rename(tempPath, targetPath, err => {
+          if (err) return handleError(err, res);
+  
+          res
+            .status(200)
+            .contentType("text/plain")
+            .end("File uploaded!");
+        });
+      } else {
+        fs.unlink(tempPath, err => {
+          if (err) return handleError(err, res);
+  
+          res
+            .status(403)
+            .contentType("text/plain")
+            .end("Only .png files are allowed!");
+        });
+      }
+});
+
+app.get("/image.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "./images/Screenshot (440).png"));
+  });
+
+
+app.listen(3000);
+
+
+
+
+
 
 
 
